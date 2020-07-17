d536c3d854bdd5c0e014f9c5a3403e11e812e655
深渊为陵 <53933714+f-lk@users.noreply.github.com>
2020-07-17 22:53:14 +0800

    Add files via upload

---
+++ 第七讲xiezuoye_directBA/cmake-build-debug/CMakeFiles/directBA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/directBA.dir/directBA.cpp.o"
+  "directBA.pdb"
+  "directBA"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/directBA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
